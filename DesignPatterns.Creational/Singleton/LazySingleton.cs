13e9c28818f2da81fdac7f0a185d6a97d37e632e
rjn0n1 <really4f@gmail.com>
2026-01-11 13:41:30 +0530

    'Creational/Singleton/Lazy'

---
+++ DesignPatterns.Creational/Singleton/LazySingleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Singleton
+{
+    public sealed class LazySingleton //sealed prevents inheritance that can break the Singleton guarantee.instance control,
+    {
+        //create an instace var
+        private static LazySingleton lazySingleton;
+
+        public static LazySingleton Instance //public get property
+        {
+            get
+            {
+                if (lazySingleton == null)
+                {
+                    lazySingleton = new LazySingleton();
+                }
+                return lazySingleton;
+            }
+        }
+        private LazySingleton()  //private prevents the instanciation
+        {
+
+        }
+    }
+}
