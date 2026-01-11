3bcf5adf8d35ea0dcec2bf8556ddfb7cfd9db72b
rjn0n1 <really4f@gmail.com>
2026-01-11 14:00:52 +0530

    Creational/Singleton-V1.0

--- DesignPattern.Demo/Program.cs
+++ DesignPattern.Demo/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DesignPatterns.Creational.Singleton;
+Console.WriteLine("LazySingleton");
+
+LazySingleton lazySingleton1 = LazySingleton.Instance;
+LazySingleton lazySingleton2 = LazySingleton.Instance;
+
+Console.WriteLine($"Instance 1 HashCode: {lazySingleton1.GetHashCode()}");
+Console.WriteLine($"Instance 2 HashCode: {lazySingleton2.GetHashCode()}");
+
+Console.WriteLine("LazySingleton");
+ThreadSafeLogger t1 = ThreadSafeLogger.Instance;
+ThreadSafeLogger t2 = ThreadSafeLogger.Instance;
+
+Console.WriteLine($"Instance 1 HashCode: {t1.GetHashCode()}");
+Console.WriteLine($"Instance 2 HashCode: {t2.GetHashCode()}");
+
+// See https://aka.ms/new-console-template for more information
+
+
---
+++ DesignPatterns.Creational/Singleton/ThreadSafeLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Singleton
+{
+    public sealed class ThreadSafeLogger //can not be exgtended.
+    {
+        static readonly Lazy<ThreadSafeLogger> _logger = new Lazy<ThreadSafeLogger>(() => new ThreadSafeLogger());
+
+        private ThreadSafeLogger() { } //no instantiation
+
+        public static ThreadSafeLogger Instance { get { return _logger.Value; } } //on demand create an instance 
+    }
+}
